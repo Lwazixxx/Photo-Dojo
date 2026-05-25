5ae6b347032bfd5f383d8603254a4a46f496ac36
lwazi Mokone <moknelwazi4@gmail.com>
2026-05-25 12:09:48 +0300

    Add initial CSS styling and JavaScript for fetching images

---
+++ main.js
@@ -0,0 +1,15 @@
+Document.addEventListener("DOMContentLoaded", () => {
+
+    const url = 'https://picsum.photos/v2/list/?page=2&limit=100';
+
+    const res = await fetch(url);
+
+    if (res.ok === true) {
+        console.log(res)
+
+    }
+
+    else {
+        alert('Error fetching data');
+    }
+});
