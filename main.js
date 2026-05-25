fb3148981ba025a3c1145551ff650e556b1b8bef
lwazi Mokone <moknelwazi4@gmail.com>
2026-05-25 14:38:48 +0300

    Update HTML structure and enhance JavaScript for dynamic image fetching

--- main.js
+++ main.js
@@ -1,15 +1,43 @@
-Document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", async () => {
 
     const url = 'https://picsum.photos/v2/list?page=2&limit=100';
 
     const res = await fetch(url);
 
+    const h1 = document.querySelector('h1');
+
+    const ancher = document.querySelector('a');
+
+
+
+
+
     if (res.ok === true) {
-        console.log(res)
+        console.log(res);
+
+        const data = await res.json();
+
+        const iNumber = (Math.round(Math.random() * 10) * 100);
+
+
+        console.log(iNumber);
+
+        const imgData = data[67];
+
+        ancher.textContent = imgData.author;
+
+        ancher.href = imgData.url;
+
+        h1.style.backgroundImage = `url(${imgData.download_url})`;
+
+
+        console.log(data);
 
     }
 
     else {
         alert('Error fetching data');
     }
+
+
 });
